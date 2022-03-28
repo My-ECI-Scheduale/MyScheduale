5362e14521c40fb58bce7ca704e571e68278df83
Kristhian David Segura <kristhian.segura@mail.escuelaing.edu.co>
2022-03-28 14:01:30 -0500

    CRUD kanbanView

--- src/main/resources/static/js/kanbanAPI.js
+++ src/main/resources/static/js/kanbanAPI.js
@@ -1,6 +1,15 @@
 var kanbanApi = (function(){
 
     var kanbanData;
+    var cont = 1;
+
+    function getCont(){
+        return cont;
+    }
+
+    function sumCont(){
+        cont += 1;
+    }
 
     function getData(kanbanId){
         $.ajax({
@@ -8,13 +17,12 @@ var kanbanApi = (function(){
             url: "http://localhost:8080/api/kanban/"+kanbanId
         }).then(function (data) {
             console.log(data);
-            var cont = 1;
             data.forEach(element => {
                 // Add first dropzone
                 $("#"+element.name).append("<div class=\"dropzone\"></div>");
                 element.items.forEach(task => {
                     var newItem = parseHtml("<div id=\"task"+cont+"\" class=\"kanban-item\">"
-                    +"<div contenteditable=\"true\" id=\"t"+cont+"\" class=\"item-input\" draggable=\"true\">"+task+"</div>"
+                    +"<div id=\"t"+cont+"\" class=\"item-input\" draggable=\"true\">"+task+"</div>"
                     +"<div class=\"dropzone\"></div>"
                     +"</div>");
                     $("#"+element.name).append(newItem);
@@ -43,6 +51,8 @@ var kanbanApi = (function(){
 
     return {
         getKanban : getData,
-        insert : insertItem
+        insert : insertItem,
+        getTaskCont: getCont,
+        sumToCont: sumCont
     }
 })();
--- src/main/resources/static/js/kanbanMain.js
+++ src/main/resources/static/js/kanbanMain.js
@@ -4,7 +4,43 @@ var kanban = (function (){
 
     function getKanbanData(){    
         module.getKanban(10);
+        loadEventListeners();
+    }
+
+    function insertItem(element){
+        var newItem = parseHtml("<div id=\"task"+module.getTaskCont()+"\" class=\"kanban-item\">"
+                    +"<div id=\"t"+module.getTaskCont()+"\" class=\"item-input\" draggable=\"true\"></div>"
+                    +"<div class=\"dropzone\"></div>"
+                    +"</div>");
+        module.sumToCont();
+        element.append(newItem);
+    }
+
+    function parseHtml(html){
+        var t = document.createElement('template');
+        t.innerHTML = html;
+        return t.content;
+    }
+
+    function loadEventListeners(){
         $(document).ready(function(){
+            // Add button + click eventListener CREATE
+            $(".kanban-column").on("click", ".add-item", function(event){
+                console.log(event);
+                console.log(event.target.parentElement.querySelector('.items'));
+                insertItem(event.target.parentElement.querySelector('.items'));
+            });
+            // Add double click eventListeners UPDATE
+            $(".items").on("dblclick", ".item-input", function(event){
+                console.log(event);
+                console.log(event.target.id);
+                if (!event.target.hasAttribute("contenteditable")) {
+                    event.target.setAttribute("contenteditable", "true");   
+                }
+                else {
+                    event.target.removeAttribute("contenteditable");
+                }
+            });
             // Add dragstart eventListeners
             $(".items").on("dragstart", ".kanban-item", function(event){
                 // To solve jquery dataTransfer issue
@@ -24,11 +60,22 @@ var kanban = (function (){
                 event.target.classList.add("dropzone-active")
                 console.log(event.target.classList);
             });
+            $(".items").on("dragover", ".delete-dropzone", function(event){
+                console.log(event);
+                event.preventDefault();
+                event.target.classList.add("delete-dropzone-active")
+                console.log(event.target.classList);
+            });
             // Add dragLeave event to remove the dropzone-active class
             $(".items").on("dragleave", ".dropzone", function(event){
                 console.log(event);
                 event.preventDefault();
                 event.target.classList.remove("dropzone-active")
+            });
+            $(".items").on("dragleave", ".delete-dropzone", function(event){
+                console.log(event);
+                event.preventDefault();
+                event.target.classList.remove("delete-dropzone-active")
             });
             // Add drop eventListeners
             $(".items").on("drop", ".dropzone", function(event){
@@ -42,17 +89,23 @@ var kanban = (function (){
                 console.log(event.dataTransfer.getData("text/plain"));
                 const droppedElement = document.getElementById(droppedElementId);
                 insertAfter.after(droppedElement);
-            }); 
-        });   
-    }
-
-    function parseHtml(html){
-        var t = document.createElement('template');
-        t.innerHTML = html;
-        return t.content;
+            });
+            // Add drop delete eventListeners
+            $(".items").on("drop", ".delete-dropzone", function(event){
+                event.preventDefault();
+                console.log(event);
+                event.target.classList.remove("delete-dropzone-active")
+                // Get the dropped element and append
+                const droppedElementId = event.dataTransfer.getData("text/plain");
+                console.log(event.dataTransfer.getData("text/plain"));
+                const droppedElement = document.getElementById(droppedElementId);
+                droppedElement.remove();
+            });  
+        });
     }
 
     return {
         read: getKanbanData,
     }
+
 })();
